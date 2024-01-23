5432a5c2659c89d1306a64fe7068037b990e27d8
lucasfanech <fanech.lucas@gmail.com>
2024-01-23 11:18:24 +0100

    detect language

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
-from perplexity import Perplexity
-#https://github.com/nathanrchn/perplexityai
 import pyttsx3
+from langdetect import detect
+from perplexity import Perplexity
 
 perplexity = Perplexity("fanechlucas@gmail.com")
-answer = perplexity.search("Quel est le sens de la vie?")
+answer = perplexity.search("What is the weather like today?")
 response = ""
 for a in answer:
     response += a['answer']
 
 print(response)
+
 # Initialiser le moteur de synthèse vocale
 engine = pyttsx3.init()
 
-# Changer la langue en français
-engine.setProperty('voice', 'fr')  # Changer 'fr' par le code de la langue souhaitée
+# Détecter la langue du texte
+langue = detect(response)
+print("Langue choisie:", langue)
+# Changer la langue du moteur de synthèse vocale
+engine.setProperty('voice', langue)  # Utilise la langue détectée
 
 # Convertir le texte en discours
 engine.say(response)
